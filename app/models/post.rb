69507aae4d60c6c4375702f46006130a21df5298
erikathea <ethajes@gmail.com>
2016-12-16 01:47:15 +0800

    Small UI fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   has_many :attachments, as: :attachable, dependent: :destroy
   has_many :pictures, as: :imageable, dependent: :destroy
   validates :title, presence: true
+
+  def content_preview
+    content[0..75] + '...' unless content.empty?
+  end
 end
